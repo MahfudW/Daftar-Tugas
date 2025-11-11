06f13c2bb917d2a33f4402fa97e94b331157b04f
MahfudDicoding <ahmedtsar80@gmail.com>
2025-11-11 09:56:52 +0700

    Typo dari Todo Harusnya Todos

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   // simpan ke localStorage setiap kali todos berubah
   useEffect(() => {
-    localStorage.setItem("todo", JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
   const addTodo = (newTodo) => {
