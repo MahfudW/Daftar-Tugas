bb0895827eb3eca8df2116fa6199bf76e5d777eb
AhmedTsar <yudimaulana1701@yahoo.com>
2025-12-21 08:06:11 +0700

    typo dari 2x1 ke 2xl

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <h2 className="text-2x1 font-bold text-center text-blue-600">
+        <h2 className="text-2xl font-bold text-center text-blue-600">
             Daftar Tugas
         </h2>
     );
